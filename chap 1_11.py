e64daebb7d13fa3966a96249387fc4d5d4afec3e
Talha-Jawed <jawedtalha0@gmail.com>
2019-11-03 19:32:59 +0500

    chapter 1_11

---
+++ chap 1_11.py
@@ -0,0 +1,40 @@
+msg = "My Name is Talha"
+print(msg)
+
+# Numbers
+number = 123456
+number += 1
+print(number)
+concatination = "19" + "97"
+print(concatination)
+
+# Bodmas rules
+num = 4 * 8 / 2 + 10  # Division single / show result in point(.) & double not show point(.)
+print(num)
+
+
+# Concatenating strings
+firstName = "Talha"
+lastName = "Javed"
+fullName = firstName + ' ' + lastName
+print(fullName)
+
+greeting = "Hello"
+space = " "
+address = "World"
+separators = "!"
+whole_greeting = greeting + space + address + separators
+print(whole_greeting)
+
+# if else statment
+if ('hello' == 'hello'):
+    print('condition true')
+else:
+    print('condition false')
+
+if (2 > 2):
+    print("Talha")
+elif ('python' == 'javaScript'):
+    print("elif ")
+else:
+    print("else")
