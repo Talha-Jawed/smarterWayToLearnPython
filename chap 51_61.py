fd1e6fbaf4e94e552b26edb00afb28b5ba5d2a47
Talha-Jawed <jawedtalha0@gmail.com>
2020-01-04 13:45:27 +0500

    Assignment no: 6

--- chap 51_61.py
+++ chap 51_61.py
@@ -1,10 +1,10 @@
-# chapter 51 - 61
+# chapter 51 - 61 and assignment no: 6
 
 # How to use while loop python same as javscript
 num = 0
-newName,newPage = "hi",22
+newName, newPage = "hi", 22
 print(newPage)
-print(newName) 
+print(newName)
 while num < 5:
     print("hello world")
     num += 1
@@ -13,17 +13,18 @@ while num < 5:
 class Person:
     def __init__(self, fullName):  # this is class constructor
         self.name = fullName  # self is like "this" of javascript
+
     def nameofPerson(self):
         print(self.name)
 
 
-Talha = Person("Talha Javed")
-Talha.nameofPerson()
+givenName = Person("Talha Javed") 
+givenName.nameofPerson()
 
 # It also pass referanc just like javascript
-newMember = Talha
+newMember = givenName
 print(newMember.name)
-Talha.name = "Talha"
+givenName.name = "Talha"
 print(newMember.name)
 a = 55
 b = a
@@ -31,3 +32,34 @@ print(b)
 a = 70
 print(b)
 print(a)
+
+
+#  Assignment no: 6
+
+class car(object):
+
+    def __init__(self, color, vehicle, person):
+        self.color = color
+        self.vehicle = vehicle
+        self.person = person
+
+    def start(self):
+        # %s is like `${this.value}` of javascript
+        return "%s starting" % self.vehicle
+
+    def brake(self):
+        return "%s braking" % self.vehicle
+
+    def drive(self):
+        return "I am driving a %s %s! with %s person" % (self.color, self.vehicle, self.person)
+
+
+civic = car("black", "civic", 3)
+print(civic.brake())
+print(civic.start())
+print(civic.drive())
+corolla = car("silver", "corolla", 3)
+print(corolla.drive())
+fortuner = car("white", "fortuner", 4)
+print(fortuner.drive())
+print(fortuner.brake())
